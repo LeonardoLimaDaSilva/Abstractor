c3c380d97994f17eb63a148d6c6d758d04c574f0
Leonardo Lima da Silva <leonardolimadasilva@live.com>
2015-10-31 08:53:20 -0300

    Inclusão IMemento

---
+++ Abstractor.Cqrs/Interfaces/Domain/IMemento.cs
@@ -0,0 +1,9 @@
+﻿namespace Abstractor.Cqrs.Interfaces.Domain
+{
+    /// <summary>
+    /// Representa o estado interno de uma entidade.
+    /// </summary>
+    public interface IMemento
+    {
+    }
+}
