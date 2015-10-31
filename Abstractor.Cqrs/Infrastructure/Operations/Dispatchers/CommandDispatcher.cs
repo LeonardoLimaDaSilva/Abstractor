797b5f703e3d9f4d89e28a4699afc3617f396fcd
Leonardo Lima da Silva <leonardolimadasilva@live.com>
2015-10-31 08:52:37 -0300

    Exclusão de classes desnecessárias

--- Abstractor.Cqrs/Infrastructure/Operations/Dispatchers/CommandDispatcher.cs
+++ Abstractor.Cqrs/Infrastructure/Operations/Dispatchers/CommandDispatcher.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Abstractor.Cqrs.Interfaces.CompositionRoot;
 using Abstractor.Cqrs.Interfaces.Operations;
 
@@ -13,16 +15,22 @@ namespace Abstractor.Cqrs.Infrastructure.Operations.Dispatchers
             _container = container;
         }
 
+        [DebuggerStepThrough]
         public void Dispatch(ICommand command)
         {
+            if (command == null) throw new ArgumentNullException("command");
+
             var handlerType = typeof(ICommandHandler<>).MakeGenericType(command.GetType());
             dynamic handler = _container.GetInstance(handlerType);
 
             handler.Handle((dynamic)command);
         }
 
+        [DebuggerStepThrough]
         public async Task DispatchAsync(ICommand command)
         {
+            if (command == null) throw new ArgumentNullException("command");
+
             var handlerType = typeof(ICommandHandler<>).MakeGenericType(command.GetType());
             dynamic handler = _container.GetInstance(handlerType);
 
--- Abstractor.Cqrs/Infrastructure/Operations/Dispatchers/QueryDispatcher.cs
+++ Abstractor.Cqrs/Infrastructure/Operations/Dispatchers/QueryDispatcher.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Abstractor.Cqrs.Interfaces.CompositionRoot;
 using Abstractor.Cqrs.Interfaces.Operations;
 
@@ -12,17 +14,23 @@ namespace Abstractor.Cqrs.Infrastructure.Operations.Dispatchers
         {
             _container = container;
         }
-        
+
+        [DebuggerStepThrough]
         public TResult Dispatch<TResult>(IQuery<TResult> query)
         {
+            if (query == null) throw new ArgumentNullException("query");
+
             var handlerType = typeof(IQueryHandler<,>).MakeGenericType(query.GetType(), typeof(TResult));
             dynamic handler = _container.GetInstance(handlerType);
 
             return handler.Handle((dynamic)query);
         }
-        
+
+        [DebuggerStepThrough]
         public async Task<TResult> DispatchAsync<TResult>(IQuery<TResult> query)
         {
+            if (query == null) throw new ArgumentNullException("query");
+
             var handlerType = typeof(IQueryAsyncHandler<,>).MakeGenericType(query.GetType(), typeof(TResult));
             dynamic handler = _container.GetInstance(handlerType);
 
