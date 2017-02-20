d5462440c628f734824f0da53af9ebced102a7e3
Leonardo Lima da Silva <leonardolimadasilva@live.com>
2017-02-20 16:08:19 -0300

    Added integration of SimpleInjector and Web Api.

---
+++ Abstractor.Owin.WebApi.SimpleInjector/ContainerFactory.cs
@@ -0,0 +1,29 @@
+﻿using Abstractor.Cqrs.SimpleInjector.Adapters;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+
+namespace Abstractor.Owin.WebApi.SimpleInjector
+{
+    /// <summary>
+    ///     Factory for SimpleInjector container adapter.
+    /// </summary>
+    public static class ContainerFactory
+    {
+        private static Container _container;
+
+        /// <summary>
+        ///     Singleton instance of SimpleInjector underlying container.
+        /// </summary>
+        public static Container Container => _container ?? (_container = new Container());
+
+        /// <summary>
+        ///     Creates a new instance of container adapter with the default scoped Web Api Lifestyle.
+        /// </summary>
+        /// <returns>An adapter.</returns>
+        public static ContainerAdapter CreateAdapter()
+        {
+            Container.Options.DefaultScopedLifestyle = new WebApiRequestLifestyle();
+            return new ContainerAdapter(Container);
+        }
+    }
+}
---
+++ Abstractor.Owin.WebApi.SimpleInjector/HttpConfigurationExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+
+namespace Abstractor.Owin.WebApi.SimpleInjector
+{
+    /// <summary>
+    ///     Extends the HttpConfiguration class.
+    /// </summary>
+    public static class HttpConfigurationExtension
+    {
+        /// <summary>
+        ///     Registers the Web Api controllers and defines the SimpleInjector as the dependency resolver.
+        /// </summary>
+        /// <param name="httpConfiguration"></param>
+        public static void UseSimpleInjector(this HttpConfiguration httpConfiguration)
+        {
+            ContainerFactory.Container.RegisterWebApiControllers(httpConfiguration);
+            ContainerFactory.Container.Verify();
+
+            httpConfiguration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(ContainerFactory.Container);
+        }
+    }
+}
